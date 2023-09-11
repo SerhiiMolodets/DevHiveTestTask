ea5e96534857f352b9976f998b7ec0c3b836edc2
SerhiiMolodets <mr.molodets@gmail.com>
2023-09-11 23:09:51 +0300

    added project;

---
+++ Task #1/PostsApp/PostsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PostsApp
+//
+//  Created by Serhii Molodets on 11.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
